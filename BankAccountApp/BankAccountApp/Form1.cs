4b19b126f695895f3c8262ac1092b84f33e93316
riba <ribasuleman97@gmail.com>
2025-11-05 15:32:37 +0500

    1st data

---
+++ BankAccountApp/BankAccountApp/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankAccountApp
+{
+    public partial class Form1 : Form
+    {
+        List<BankAccount> AllAccount = new List<BankAccount>();
+    
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+                return;
+
+            BankAccount bankaccount1 = new BankAccount(textBox1.Text);
+
+            AllAccount.Add(bankaccount1);
+            RefreshGuid();
+            MessageBox.Show("AccountCreateSuccessfully");
+
+
+        }
+        public void RefreshGuid()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = AllAccount;
+            textBox1.Text = "";
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            0BankAccount SelectAccount = dat1aGridView1.SelectedRows[0].DataBoundItem as BankAccount;
+            SelectAccount.Balance += numericUpDown1.Value;
+            RefreshGuid();
+            numericUpDown1.Value = 0;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BankAccount SelectAccount = dataGridView1.SelectedRows[0].DataBoundItem as BankAccount;
+            decimal withdrawAmount = numericUpDown1.Value;
+          
+
+            if (SelectAccount.Balance < withdrawAmount)
+            {
+                MessageBox.Show("insufficient balance");
+                return;
+            }
+
+            SelectAccount.Balance -= numericUpDown1.Value;
+            RefreshGuid();
+            numericUpDown1.Value = 0;
+
+        }
+
+    }
+}
---
+++ BankAccountApp/BankAccountApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BankAccountApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankAccountApp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd2fb6b2-b0fa-45de-8ec2-7b54e405d7b9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
